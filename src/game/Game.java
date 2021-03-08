99db10aae56516acf8d4756c06c249f8c74262f9
aya-lakehal <aya-lakehal40@outlook.fr>
2021-03-08 10:03:47 +0100

    Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -27,7 +27,7 @@ public abstract class Game {
 	 * public constructor for the game class
 	 */
 	public Game() {
-		map = new Map("testMap", 5, 5);
+		map = new Map("testMap", 8, 8);
 		createPlayers();
 		// chose the first player in a random way
 		Random r = new Random();
