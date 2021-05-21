624ade46757a04ecbce427d46e6273b4a11d30f8
ziko hb <zakaria64@sfr.fr>
2021-05-21 16:02:53 +0200

    ajout d'un test

--- test/game/util/PositionTest.java
+++ test/game/util/PositionTest.java
@@ -13,9 +13,46 @@ import org.junit.Test;
  */
 public class PositionTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		Position PosTest = new Position(4, 7);
+		Position PosTest2 = new Position(2, 5);
+	}
+	
+	public void toStringWW() {
+		assertEquals("(4,7)", PosTest.toString());
+	}
+
+	public void getXCoordinateWW() {
+		assertEquals(4, PosTest.getXCoordinate());
+	}
+
+	public int getYCoordinateWW() {
+		assertEquals(7, PosTest.getYCoordinate());
+	}
+
+	public void setPositionWW() {
+		assertEquals(4, PosTest.getXCoordinate());
+		assertEquals(7, PosTest.getYCoordinate());
+		PosTest.setPosition(6, 8);
+		assertEquals(6, PosTest.getXCoordinate());
+		assertEquals(8, PosTest.getYCoordinate());
+	}
+
+	public void setXCoordinateWW() {
+		assertEquals(4, PosTest.getXCoordinate());
+		PosTest.setXCoordinate(5);
+		assertEquals(5, PosTest.getXCoordinate());
+	}
+
+	public void setYCoordianteWW() {
+		assertEquals(7, PosTest.getYCoordinate());
+		PosTest.setYCoordinate(3);
+		assertEquals(3, PosTest.getYCoordinate());
+	}
+	
+	public void calculateDistanceWW() {
+		assertEquals(Math.sqrt(8), PosTest.calculateDistance(PosTest2));
 	}
 
 }
